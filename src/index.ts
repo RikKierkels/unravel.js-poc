2afb18b6748639c31ceea29b5913ebe53a7df88e
Jordy Langen <jordylangen@gmail.com>
2020-09-15 20:59:12 +0200

    Format the output so its easier to verify of the dependencies are correctly found (#1)
    * Format the output so its easier to verify of the dependencies are correctly found
    
    * Removed unused import
    
    * Only search based on the root dir

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { readFile } from 'fs';
 import { sync } from 'glob';
 import { join, dirname, parse as parsePath } from 'path';
 import { Node } from '@babel/types';
+import { cloneDeep, last } from 'lodash';
 import { parse } from '@babel/parser';
 import { detectImportDeclaration, Detector, detectRequireCallExpression } from './detect';
 import visit, { Ast } from './visit';
+import chalk from 'chalk';
 
 const readFileAsync = (path: string): Promise<string> =>
   new Promise((resolve) => readFile(path, 'utf-8', (err, data) => (err ? resolve('') : resolve(data))));
@@ -15,21 +17,23 @@ type Dependency = {
 };
 
 type Module = {
+  name: string;
   path: string;
   // TODO: Other name? Confusing as you expect an array of type Dependency here.
   dependencies: Module[];
 };
 
 type Options = {
+  root: string;
   detectors: Detector[];
   ignore: string[];
 };
 
-async function run(patterns: string[], { detectors, ignore }: Options) {
+async function run(patterns: string[], { detectors, ignore, root }: Options) {
   let dependencies: Dependency[] = (
     await Promise.all(
       patterns
-        .flatMap((pattern) => match(pattern, ignore))
+        .flatMap((pattern) => match(pattern, ignore, root))
         .reduce<string[]>((paths, path) => (paths.includes(path) ? paths : [...paths, path]), [])
         .map((path) => getDependencies(detectors, path)),
     )
@@ -37,23 +41,24 @@ async function run(patterns: string[], { detectors, ignore }: Options) {
     .flat()
     .map(({ from, to }) => ({ from: withoutFileExtension(from), to: withoutFileExtension(to) }));
 
-  const modules = mapToUniqueModules(dependencies).map((module, _, modules) => {
+  const modules = mapToUniqueModules(dependencies, root).map((module, _, modules) => {
     module.dependencies = dependencies
       .filter(({ from }) => from === module.path)
-      .flatMap(({ to }) => modules.find((module) => module.path === to) || []);
+      .flatMap(({ to }) => modules.find((module) => module.path === to) || [])
+      .map((modules) => cloneDeep(modules));
     return module;
   });
 
-  console.log(modules);
+  modules.forEach((module) => print([module]));
 }
 
-function match(pattern: string, ignore: string[] = []): string[] {
-  return sync(pattern, { absolute: true, ignore, nodir: true });
+function match(pattern: string, ignore: string[] = [], root: string): string[] {
+  return sync(pattern, { absolute: true, ignore, nodir: true, root });
 }
 
 function withoutFileExtension(path: string): string {
-  const { root, dir, name } = parsePath(path);
-  return join(root, dir, name);
+  const { dir, name } = parsePath(path);
+  return join(dir, name);
 }
 
 async function getDependencies(detectors: Detector[], filepath: string): Promise<Dependency[]> {
@@ -81,20 +86,45 @@ function resolveRelativeTo(path: string, otherPath: string): string {
   return join(dirname(path), otherPath);
 }
 
-function mapToUniqueModules(dependencies: Dependency[]): Module[] {
+function mapToUniqueModules(dependencies: Dependency[], rootDirectory: string): Module[] {
   return dependencies.reduce<Module[]>(
     (modules, { from, to }) => [
       ...modules,
       ...[from, to]
         .filter((path) => !modules.some((module) => module.path === path))
-        .map((path) => ({ path, dependencies: [] })),
+        .map((path) => ({ name: getModuleName(rootDirectory, path), path, dependencies: [] })),
     ],
     [],
   );
 }
 
+function getModuleName(rootDirectory: string, path: string): string {
+  const { dir, name } = parsePath(path);
+  return join(last(dir.split(rootDirectory))!, name);
+}
+
+function print(modules: Module[], indentation: number = 0): void {
+  const isRootModule = indentation === 0;
+
+  modules.forEach((module) => {
+    if (isRootModule) {
+      console.log(chalk.inverse(module.name));
+    } else {
+      const prefix = '-'.repeat(indentation) + '→ ';
+      console.log(chalk.white(`${prefix}${module.name}`));
+    }
+
+    print(module.dependencies, indentation + 1);
+  });
+
+  if (isRootModule) {
+    console.log('\n');
+  }
+}
+
 (async () =>
   await run(['src/test-modules/**'], {
+    root: 'src',
     detectors: [detectImportDeclaration, detectRequireCallExpression],
     ignore: ['src/test-modules/module-c.js'],
   }))();
