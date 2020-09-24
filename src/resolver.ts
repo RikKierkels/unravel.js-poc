fde5c365eb8d6a81b594d906de898911504cbe15
Rik Kierkels <kierkels.r@gmail.com>
2020-09-24 21:54:14 +0200

    Remove calls to require resolve

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,14 +1,19 @@
-import { dirname, join } from 'path';
+import path from 'path';
 
-export function resolve(filePath: string, dependency: string): string {
-  try {
-    require.resolve(dependency);
-    return dependency;
-  } catch {
-    return resolveRelativeTo(filePath, dependency);
-  }
+export function resolve(modulePath: string, dependency: string): string {
+  const isPackageModule = !isAbsolute(dependency) && !isRelative(dependency);
+  return isPackageModule ? dependency : resolveRelativeTo(modulePath, dependency);
 }
 
-function resolveRelativeTo(path: string, otherPath: string): string {
-  return join(dirname(path), otherPath);
+function isAbsolute(modulePath: string): boolean {
+  return path.resolve(modulePath) === path.normalize(modulePath);
+}
+
+function isRelative([firstChar]: string): boolean {
+  // TODO: Determine edge cases
+  return firstChar === '.';
+}
+
+function resolveRelativeTo(modulePath: string, otherModulePath: string): string {
+  return path.join(path.dirname(modulePath), otherModulePath);
 }
